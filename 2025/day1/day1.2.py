4f79fd316ddbc67a7044cea55d76d8263ab4ea8b
scottbarbieri <scottbarbieri@users.noreply.github.com>
2025-12-01 21:17:46 +0000

    completed day 1

---
+++ 2025/day1/day1.2.py
@@ -0,0 +1,24 @@
+import math
+with open("/workspaces/AofC/2025/day1/input.txt", "r") as file:
+    input = file.read().splitlines()
+count =0
+dial = 50
+code = 0
+for turns in input:
+    count += 1
+    direction = turns.rstrip("0123456789")
+    if direction == "L":
+        for i in range(int(turns.lstrip("LR"))):
+            dial = dial -1
+            if dial == 0:
+                code += 1
+            if dial < 0: 
+                dial = 99
+    elif direction == "R":
+        for i in range(int(turns.lstrip("LR"))):
+            dial += 1
+            if dial > 99: 
+                dial = 0
+                code += 1
+    print (count, dial)
+print (code)
